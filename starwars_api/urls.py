ef9db689068943f08a018576a82f596532f61be5
Evangelos Katsaros <e_katsaros@hotmail.com>
2025-06-28 20:30:16 +0300

    Co-pilot suggested changes.

--- starwars_api/urls.py
+++ starwars_api/urls.py
@@ -18,4 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/user/", include("users.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/user/", include("users.urls")),
+]
